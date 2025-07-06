0fdf4b1cf5781b01a6c2b6b7b5ee7b90b341e18a
Sepehr Akbari <isepehrakbari@gmail.com>
2025-07-06 13:36:36 -0500

    Create AxisOfSymmetry.py

---
+++ scripts/AxisOfSymmetry.py
@@ -0,0 +1,13 @@
+image = import["sepehrakbari/documents/IB/MathIA/egg_bg.png"]
+image = FillingTransform[Closing[image, 2]]
+
+comp = componentMeasurement[
+    image, 
+    {"Centroid", "Orientation", "Length"}
+    ]
+
+HighlightImage[img,
+               comp /. (index -> {centroid_, orientation_, length_}) :> {
+                   Rotate[Line[{centroid - {length/2, 0}, centroid + {length/2, 0}}],
+                   orientation]
+               }]
